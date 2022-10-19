3ecf3e71ff2e9cefb755d91cf4a9223048a0a577
Jignasu <1jignasu@gmail.com>
2022-10-19 19:16:04 -0400

    Added frontend domain in allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins =[
     'https://www.yoursite.com', 
     'http://127.0.0.1:5500', 
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://isa-vt.herokuapp.com/'
 ]; //whatever webapp that we want to give access to to this as backend
 
 module.exports = allowedOrigins;
